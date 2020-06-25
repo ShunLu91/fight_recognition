56bd565817a8d21b2c2520a6b2202674036dd811
lushun_mac <470651748@qq.com>
2020-06-25 21:09:54 +0800

    apex batch=48

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
-#nohup python -u train.py --model R2Plus1D --epoch 100 > logdir/r0.log  2>&1 &
-python -u train.py --model R2Plus1D --epoch 100
-
+#nohup python -u train.py --model R2Plus1D --epoch 100 --batchsize 16 > logdir/r0.log  2>&1 &
+nohup python -u train.py --model R2Plus1D --epoch 100 --batchsize 48 > logdir/r1.log  2>&1 &
